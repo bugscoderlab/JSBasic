a710264cb1591c2b032f4c7996ef981f70a965d1
bugscoder-github <73679325+bugscoder-github@users.noreply.github.com>
2025-09-05 19:18:11 +0800

    func

--- array.js
+++ array.js
@@ -1,12 +1,63 @@
-let students = [
-    { name: 'Alice', grade: 85 },
-    { name: 'Bob', grade: 92 },
-    { name: 'Charlie', grade: 78 }
-]
-console.log(students)
-console.log(students[0].name);
-console.log(students[2].name);
-console.log(students[3]?.grade);
+// exercise: convert // function say(name, age) {
+//     console.log(`My name is ${name}. I'm ${age} this year`)
+// }
+//  to arrow function
+
+// const say = (name, age) => {
+//     return `My name is ${name}. I'm ${age} this year`
+// }
+// console.log(say("bond", 42))
+
+
+// const sayBob = () => (
+//     "Bob"
+// )
+
+// console.log(sayBob());
+
+// exercise: loop an array and call a function, pass value into parameter from array value
+
+// function say(name, age) {
+//     console.log(`My name is ${name}. I'm ${age} this year`)
+// }
+// say("Tat", 42)
+
+
+// function sayBob() {
+//     console.log("Bob");
+// }
+
+
+
+
+// exercise: loop an array
+
+
+// for (let x=0; x<=10; x++) {
+//     if (x == 2) { continue; }
+//     if (x == 5) { break; }
+
+//     console.log(x);
+// }
+
+
+
+
+// let students = [
+//     { name: 'Alice', grade: 85 },
+//     { name: 'Bob', grade: 92 },
+//     { name: 'Charlie', grade: 78 }
+// ]
+// for (let x=0; x<students.length; x++) {
+//     console.log(x, students[x].name, students[x].grade)
+// }
+// students.forEach((val, key) => {
+//     console.log(key, val.name, val.grade)
+// })
+// console.log(students)
+// console.log(students[0].name);
+// console.log(students[2].name);
+// console.log(students[3]?.grade);
 
 
 
